f37f5030e791a002447fea1f4147a0f886a054a8
Abu Belal <abu.belal@clarksons.com>
2019-05-17 17:42:05 +0100

    test function

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os/exec"
+
+	"github.com/tidwall/gjson"
+)
 
 func main() {
 
@@ -10,6 +16,28 @@ func main() {
 	// 	},
 	// })
 
-	c := NewClient("fff")
-	fmt.Print(c)
+	abutest()
+
+	//c := NewClient("fff")
+	//fmt.Print(c)
+}
+
+func abutest() {
+
+	args := []string{"account", "show"}
+	out, err := exec.Command("az", args...).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output := string(out)
+
+	if gjson.Valid(output) {
+		log.Print("az cli output is valid json")
+	} else {
+		panic("az cli output not valid json")
+	}
+
+	value := gjson.Get(output, "name")
+	fmt.Print(value)
 }
